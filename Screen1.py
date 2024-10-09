a1ea2b8c8d0da0aa56ad9aba7eefd684a5315ead
miriam <miriammichaeli12@gmail.com>
2024-10-09 16:53:01 +0300

    add buttons

--- Screen1.py
+++ Screen1.py
@@ -1,35 +1,30 @@
 import consts
+from tkinter import *
 import gtts
 import time
-from tkinter import *
 import playsound3
-
-root = Tk()
-
-root.title('Teach and Reach')
-root.geometry(f'{consts.WINDOW_WIDTH}x{consts.WINDOW_HEIGHT}')
-
 
 # def text_to_speech():
 #     tts = gtts.gTTS("Welcome to Teach and Reach")
 #     tts.save('Welcome.mp3')
 #     playsound3.playsound("Welcome.mp3")
 
-
-def button_clicked():
-    print("Button clicked!")
+isTeacher = False
 
 
-def create_button():
+def teacher_clicked():
+    return True
 
+
+def create_teacher_button(root):
     button = Button(root,
-                    text="Student",
-                    command=button_clicked,
+                    text='Teacher',
+                    command=teacher_clicked,
                     activebackground='blue',
                     activeforeground="white",
                     anchor="center",
                     bd=3,
-                    bg="lightgray",
+                    bg="royalblue",
                     cursor="hand2",
                     disabledforeground="gray",
                     fg="black",
@@ -41,13 +36,35 @@ def create_button():
                     justify="center",
                     overrelief="raised",
                     padx=10,
-                    pady=5,
+                    pady=20,
                     width=15,
                     wraplength=100)
 
-    button.pack(padx=20, pady=20)
+    button.pack(padx=100, pady=100)
 
-create_button()
 
-root.mainloop()
+def create_student_button(root):
+    button = Button(root,
+                    text='Student',
+                    command=teacher_clicked,
+                    activebackground='blue',
+                    activeforeground="white",
+                    anchor="center",
+                    bd=3,
+                    bg="royalblue",
+                    cursor="hand2",
+                    disabledforeground="gray",
+                    fg="black",
+                    font=("Arial", 12),
+                    height=2,
+                    highlightbackground="black",
+                    highlightcolor="green",
+                    highlightthickness=2,
+                    justify="center",
+                    overrelief="raised",
+                    padx=10,
+                    pady=15,
+                    width=15,
+                    wraplength=100)
 
+    button.pack(padx=100, pady=100)
---
+++ main.py
@@ -0,0 +1,20 @@
+import Screen1
+import consts
+from tkinter import *
+
+root = Tk()
+
+root.title('Teach and Reach')
+root.geometry(f'{consts.WINDOW_WIDTH}x{consts.WINDOW_HEIGHT}')
+root.configure(background='lightblue')
+
+Screen1.create_student_button(root)
+Screen1.create_teacher_button(root)
+
+
+
+
+
+
+
+root.mainloop()
