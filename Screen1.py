64856627489d33df2a0b0790420e19e46c3d3d93
miriam <miriammichaeli12@gmail.com>
2024-10-09 16:11:56 +0300

    add changing to tkinter screen

--- Screen1.py
+++ Screen1.py
@@ -1,34 +1,67 @@
-import pygame
 import consts
 import gtts
 import time
+from tkinter import *
+import playsound3
 
-screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+root = Tk()
 
-
-def text_to_speech():
-    tts = gtts.gTTS("Welcome to Teach and Reach")
-    tts.save('Welcome.mp3')
-    pygame.mixer.init()
-    pygame.mixer.music.load("Welcome.mp3")
-    pygame.mixer.music.play()
-    time.sleep(2)
+root.title('Teach and Reach')
+root.geometry(f'{consts.WINDOW_WIDTH}x{consts.WINDOW_HEIGHT}')
 
 
-def create_opening_screen():
-    pygame.display.set_caption('Teach and Reach')
-    screen.fill(consts.BACKGROUND_COLOR)
-    pygame.display.flip()
-    text_to_speech()
+# def text_to_speech():
+#     tts = gtts.gTTS("Welcome to Teach and Reach")
+#     tts.save('Welcome.mp3')
+#     playsound3.playsound("Welcome.mp3")
 
 
+def button_clicked():
+    print("Button clicked!")
 
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+def create_button():
+    root = Tk()
+    button = Button(root,
+                    text="Student",
+                    command=button_clicked,
+                    activebackground='blue',
+                    activeforeground="white",
+                    anchor="center",
+                    bd=3,
+                    bg="lightgray",
+                    cursor="hand2",
+                    disabledforeground="gray",
+                    fg="black",
+                    font=("Arial", 12),
+                    height=2,
+                    highlightbackground="black",
+                    highlightcolor="green",
+                    highlightthickness=2,
+                    justify="center",
+                    overrelief="raised",
+                    padx=10,
+                    pady=5,
+                    width=15,
+                    wraplength=100)
+
+    button.pack(padx=20, pady=20)
 
 
-create_opening_screen()
+text_to_speech()
+root.mainloop()
+# def create_opening_screen():
+#     pygame.display.set_caption('Teach and Reach')
+#     screen.fill(consts.BACKGROUND_COLOR)
+#     pygame.display.flip()
+#     text_to_speech()
+#     create_button()
+#
+#     running = True
+#     while running:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 running = False
+
+
+# pygame.font.SysFont('Calibri', 20))
