da3ad51ff2ad123f343c3e03acf1e9c7914666e3
jbt <mayayerushalmi1@gmail.com>
2024-10-09 16:09:38 +0300

    Screen2

--- Screen2.py
+++ Screen2.py
@@ -6,11 +6,13 @@ list=[]
 list=["name","subject","gender","age", "description"]
 
 
-def text_box(text):
+def text_box(text,ending_text):
     window = tk.Tk()
     window.geometry("300x300")
     tk.Label(text=f"Enter {text}").pack()
     tk.Entry().pack()
+    exit_button = tk.Button(window, text=ending_text, command=window.destroy)
+    exit_button.pack(pady=20)
     window.mainloop()
 
 
@@ -18,6 +20,9 @@ def text_box(text):
 
 for i in range (5):
     text=list[i]
-    m= text_box(text)
+    if i!=4:
+        text_box(text,"Next")
+    else:
+        text_box(text, "Exit")
 
 
