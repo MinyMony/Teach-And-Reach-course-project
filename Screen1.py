e936276a93e9b12fc4f40d4b3db7154579b9852b
miriam <miriammichaeli12@gmail.com>
2024-10-09 14:23:40 +0300

    change screen1 and color

--- Screen1.py
+++ Screen1.py
@@ -2,10 +2,18 @@ import pygame
 import consts
 
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
-pygame.display.set_caption('Teach an Reach')
+
 
 def create_opening_screen():
+    pygame.display.set_caption('Teach an Reach')
+    screen.fill(consts.BORDER_COLOR)
+    pygame.display.flip()
 
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 
 
