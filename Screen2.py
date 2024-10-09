ea7055d79ef43e19dafeb3028e1bc2cae1ceb9ca
jbt <mayayerushalmi1@gmail.com>
2024-10-09 16:04:07 +0300

    Screen2

--- Screen2.py
+++ Screen2.py
@@ -1,11 +1,23 @@
 import consts
 from Screen1 import screen
 import tkinter as tk
+list=[]
+
+list=["name","subject","gender","age", "description"]
 
 
-window = tk.Tk()
-window.geometry("300x300")
-tk.Label(text="Enter Name").pack()
-tk.Entry().pack()
-window.mainloop()
+def text_box(text):
+    window = tk.Tk()
+    window.geometry("300x300")
+    tk.Label(text=f"Enter {text}").pack()
+    tk.Entry().pack()
+    window.mainloop()
+
+
+
+
+for i in range (5):
+    text=list[i]
+    m= text_box(text)
+
 
