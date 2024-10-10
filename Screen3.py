1cab8c422ab47d57420bec1e91905f16edf55f4e
mayay <mayayerushalmi1@gmail.com>
2024-10-10 14:04:48 +0300

    add changing to tkinter screen12

--- Screen3.py
+++ Screen3.py
@@ -1,63 +1,69 @@
-from numpy.version import short_version
-
+from tkinter import ttk
 import consts
 import tkinter as tk
 
+teachers_data = {
+    'Full Name': 0,
+    'Gender': 0,
+    'Subject': 0,
+    'Age Range': 0,
+    'Phone Number': 0,
+    'Short Explanation': 0}
+
+teacher_keys = list(teachers_data.keys())
 
 
-def on_submit(input_name, input_gender, input_age, input_subject, input_short_explanation,student_data,key_values,window):
-    list = [input_name, input_gender, input_age, input_subject, input_short_explanation]
-    for i in range(len(student_data)):
-        student_data[key_values[i]] = list[i]
+def on_submit(input_name, input_gender, input_subject, input_age_range, input_phone_number,input_short_explanation, window):
+    list = [input_name, input_gender, input_subject, input_age_range, input_phone_number,input_short_explanation]
+    for i in range(len(teachers_data)):
+        teachers_data[teacher_keys[i]] = list[i]
     window.destroy()
 
 
-def Screen2():
-
-    teachers_data= {'Full Name': 0,
-        'Gender': 0,
-        'Subject': 0,
-        'Age Range':0,
-        'Phone Number': 0,
-        'Short Explanation': 0}
-
-
-    key_values = list(teachers_data.keys())
-
+def create_teacher_screen():
     window = tk.Tk()
     window.geometry(f'{consts.WINDOW_WIDTH}x{consts.WINDOW_HEIGHT}')
     window.configure(background='lightblue')
 
     name_str = tk.StringVar()
-    tk.Label(window,text=f"Enter {key_values[0]}").pack()
+    tk.Label(text=f"Enter {teacher_keys[0]}", font=('Calibri', 20), background='lightblue').pack()
     input_name = tk.Entry(window, textvariable=name_str)
     input_name.pack()
 
     gender_str = tk.StringVar()
-    tk.Label(window,text=f"Enter {key_values[1]}").pack()
-    input_gender = tk.Entry(window, textvariable=gender_str)
+    input_gender = ttk.Combobox(window, textvariable=gender_str)
+    input_gender['values'] = ('Female',
+                              'Male', 'Non-Binary')
+    tk.Label(text=f"Enter {teacher_keys[1]}", font=('Calibri', 20), background='lightblue').pack()
     input_gender.pack()
 
-    age_int = tk.IntVar()
-    tk.Label(window,text=f"Enter {key_values[2]}").pack()
-    input_age = tk.Entry(window, textvariable=age_int)
-    input_age.pack()
-
-    subject_str = tk.StringVar()
-    tk.Label(window,text=f"Enter {key_values[3]}").pack()
-    input_subject = tk.Entry(window, textvariable=subject_str)
+    subject = tk.StringVar()
+    tk.Label(text=f"Enter {teacher_keys[2]}", font=('Calibri', 20), background='lightblue').pack()
+    input_subject = tk.Entry(window, textvariable=subject)
     input_subject.pack()
 
+    age_range = tk.StringVar()
+    tk.Label(text=f"Enter {teacher_keys[3]}", font=('Calibri', 20), background='lightblue').pack()
+    input_age_range = tk.Entry(window, textvariable=age_range)
+    input_age_range.pack()
+
+    phone_number = tk.StringVar()
+    tk.Label(text=f"Enter {teacher_keys[4]}", font=('Calibri', 20), background='lightblue').pack()
+    input_phone_number = tk.Entry(window, textvariable=phone_number)
+    input_phone_number.pack()
+
     short_explanation_str = tk.StringVar()
-    tk.Label(window,text=f"Enter {key_values[4]}").pack()
+    tk.Label(text=f"Enter {teacher_keys[5]}", font=('Calibri', 20), background='lightblue', ).pack()
     input_short_explanation = tk.Entry(window, textvariable=short_explanation_str)
     input_short_explanation.pack()
 
     exit_button = tk.Button(window, text="Submit",
-                            command=(lambda: on_submit(input_name.get(), input_gender.get(), input_age.get(),
-                                                       input_subject.get(), input_short_explanation.get(),
-                                                       student_data,key_values,window)))
+                            command=(lambda: on_submit(input_name.get(), input_gender.get(), input_subject.get(),
+                                                       input_age_range.get(), input_phone_number.get(),input_short_explanation.get(),
+                                                       window)))
     exit_button.pack(pady=20)
     window.mainloop()
 
-    print(student_data)
+
+create_teacher_screen()
+print(teachers_data)
