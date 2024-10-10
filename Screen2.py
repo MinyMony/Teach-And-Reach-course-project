a951cfb89cc2b265132a862119b7580d58c8a914
mayay <mayayerushalmi1@gmail.com>
2024-10-10 11:31:52 +0300

    add changing to tkinter screen

--- Screen2.py
+++ Screen2.py
@@ -1,5 +1,8 @@
+from numpy.version import short_version
+
 import consts
 import tkinter as tk
+
 
 student_data = {
     'Full Name': 0,
@@ -9,39 +12,34 @@ student_data = {
     'Short Explanation': 0
 }
 
+window = tk.Tk()
+window.geometry("300x300")
 
+name_str = tk.StringVar()
+gender_str=tk.StringVar()
+age_str=tk.StringVar()
+subject=tk.StringVar()
+short_version=tk.StringVar()
 
+str_list=[name_str,gender_str,age_str,subject,short_version]
 
-
-def on_submit(window, input):
-    print(input.get())
+def on_submit():
+    print(name_str.get())
     window.destroy()
 
 
 
 def text_box(dict):
-    window = tk.Tk()
-    window.geometry("300x300")
 
-    for key in dict:
-        tk.Label(text=f"Enter {key}").pack()
-        input = tk.Entry(window)
+
+    for i in range(dict):
+        print(dict[i])
+        tk.Label(text=f"Enter {dict[i]}").pack()
+        input = tk.Entry(window, textvariable=name_str)
         input.pack()
-    exit_button = tk.Button(window, text="Submit", command=(lambda :on_submit(window, input)))
+    exit_button = tk.Button(window, text="Submit", command=(lambda :on_submit()))
     exit_button.pack(pady=20)
     window.mainloop()
 
 
-#
-# window = tk.Tk()
-# window.geometry("300x300")
-#
-# tk.Label(text=f"Enter Full Name").pack()
-# input = tk.Entry(window)
-# input.grid_bbox(row=1,column=0)
-# exit_button = tk.Button(window, text="Submit", command=on_submit(window, input))
-# exit_button.pack(pady=20)
-# print(input.get())
-#
-# window.mainloop()
 text_box(student_data)
